40d31449ec61301051edfe43522eceb6b0866ec8
Amogh-1416 <samudralaamogh16@gmail.com>
2024-10-26 22:43:45 +0530

    Home started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-gray-800 backdrop-blur-md rounded-md " >
+        <nav className="sticky top-0 w-full bg-gray-900 backdrop-blur-md rounded-md " >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <a href="#" className="text-white font-bold text-xl">Synthesia</a>
